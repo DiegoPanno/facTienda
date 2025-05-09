612899659307e311a472db4b2408d0c44118a62d
DiegoPanno <diegompanno@gmail.com>
2025-05-09 09:40:09 -0300

    ajuste de useState

--- frontend/src/components/BuscadorProductos.jsx
+++ frontend/src/components/BuscadorProductos.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+
 import { obtenerProductos } from "../services/productService";
+import { useState, useEffect, useRef } from "react";
 
 const BuscadorProductos = ({ onSeleccionar }) => {
   const [query, setQuery] = useState("");
